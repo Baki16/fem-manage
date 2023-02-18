1a558f2912180bb174ae0ee48d659576548b0f5f
Baki16 <aleksandar916@gmail.com>
2023-02-19 00:49:15 +0100

    Completed header elements

---
+++ navigation.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const overlay = document.querySelector(".overlay");
+
+navToggle.addEventListener("click", () => {
+
+    overlay.classList.toggle("active")
+    
+    const visiblity = nav.getAttribute("data-visible");
+    if (visiblity === "false") {
+        nav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
+    } else {
+        nav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
+    }
+})
